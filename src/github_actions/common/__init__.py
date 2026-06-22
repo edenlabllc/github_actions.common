9c33ae0c12095254e154faf63dcc66d84bdff8b3
Bohdan Hrytsenko <b.i.gricenko@gmail.com>
2026-06-22 18:11:40 +0300

    feat: add Kubectl to module exports in __init__.py

--- src/github_actions/common/__init__.py
+++ src/github_actions/common/__init__.py
@@ -18,6 +18,7 @@ from .utils.cmd import BaseCommand, CMDInterface
 from .utils.github_environment_variables import GitHubContext
 from .utils.install_rmk import RMKInstaller
 from .utils.install_kodjin_cli import KodjinCLIInstaller
+from .utils.kubectl import Kubectl
 from .utils.keycloak import KeycloakClientInfoFetcher
 
 
@@ -49,5 +50,6 @@ __all__ = [
     "S3BucketManager",
     "SlackNotifier",
     "KodjinCLIInstaller",
+    "Kubectl",
     "GetRootDomain",
 ]
