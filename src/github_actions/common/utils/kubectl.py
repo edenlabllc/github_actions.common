f9ac85db9fa9e322a43b9e21f3407c8ebb543d35
Bohdan Hrytsenko <b.i.gricenko@gmail.com>
2026-06-22 19:11:53 +0300

    refactor: update import path and simplify get_secret method in Kubectl class

--- src/github_actions/common/utils/kubectl.py
+++ src/github_actions/common/utils/kubectl.py
@@ -1,4 +1,4 @@
-from ..utils.cmd import BaseCommand, CMDInterface
+from github_actions.common.utils.cmd import BaseCommand, CMDInterface
 
 
 class Kubectl(BaseCommand, CMDInterface):
@@ -6,38 +6,22 @@ class Kubectl(BaseCommand, CMDInterface):
         self.kubectl_download_url = kubectl_download_url
         self._install_kubectl()
 
-    def execute(self):
-        self.run()
-
-    def run(self):
-        print("Kubectl is ready to use.")
-
-    def get_secret(self, secret_name: str, namespace: str, secret_path: str) -> str:
+    def get_secret(self, secret_name: str, namespace: str, secret_path: str) -> str | None:
         try:
             print(f"Getting secret {secret_name} in namespace {namespace}.")
-            result = self.run_command(
-                f"kubectl get secret {secret_name} --namespace {namespace} --output yaml", check=True, text=True, capture_output=True
+            return self.run_command(
+                f"kubectl get secret {secret_name} --namespace {namespace} --output yaml | yq '.data.password'", capture_output=True
             )
 
-            print(f"Secret {secret_name} retrieved successfully. Extracting data.")
-            # parse yaml output and extract the password field, decode it from base64
-            result = self.run_command(
-                f"yq '.data.{secret_path} | @base64d'",
-                input=result.stdout,
-                check=True,
-                text=True,
-                capture_output=True,
-            )
-            return result.stdout
         except Exception as err:
             raise Exception(f"getting secret {secret_name} in namespace {namespace}:\n{err}")
 
     def _install_kubectl(self):
         print("Installing kubectl.")
         try:
-            self.run_command(f"bash -s -- {self.kubectl_download_url}", check=True, text=True, input="")
+            self.run_command(f"bash -s -- {self.kubectl_download_url}")
 
-            version = self.run_command("kubectl version --client", check=True, text=True, capture_output=True)
-            print(f"kubectl installed successfully. Version: {version.stdout.strip()}")
+            version = self.run_command("kubectl version --client", capture_output=True)
+            print(f"kubectl installed successfully. Version: {version}")
         except Exception as err:
             raise Exception(f"installing kubectl:\n{err}")
