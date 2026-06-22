03151bb1f09ef95ddd8a752f83946affb46fc4dc
Bohdan Hrytsenko <b.i.gricenko@gmail.com>
2026-06-22 18:09:18 +0300

    feat: refactor Kubectl class to simplify initialization and installation process

--- src/github_actions/common/utils/kubectl.py
+++ src/github_actions/common/utils/kubectl.py
@@ -2,15 +2,14 @@ import subprocess
 
 
 class Kubectl:
-    def __init__(self, args):
-        self.kubectl_version = args.kubectl_version
-        self.kubectl_download_url = args.kubectl_download_url
+    def __init__(self, kubectl_download_url="https://dl.k8s.io/release/v1.36.2/bin/linux/amd64/kubectl"):
+        self.kubectl_download_url = kubectl_download_url
         self.install_kubectl()
 
     def install_kubectl(self):
         print("Installing kubectl.")
         try:
-            subprocess.run(["bash", "-s", "--", self.kubectl_version, self.kubectl_download_url], check=True, text=True, input="")
+            subprocess.run(["bash", "-s", "--", self.kubectl_download_url], check=True, text=True, input="")
         except subprocess.CalledProcessError as err:
             raise Exception(f"installing kubectl:\n{err}")
 
@@ -19,7 +18,6 @@ class Kubectl:
             result = subprocess.run(
                 ["kubectl", "get", "secret", secret_name, "-n", namespace, "-o", "yaml"], check=True, text=True, capture_output=True
             )
-            #  kubectl get secret keycloak-cluster-initial-admin --namespace keycloak --output yaml | yq '.data.password | @base64d'
 
             # parse yaml output and extract the password field, decode it from base64
             result = subprocess.run(
